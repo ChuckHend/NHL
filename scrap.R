41d62f695009880aa4b1828c73e6438822e87602
Adam Hendel <hendel.adam@gmail.com>
2018-02-11 20:06:38 -0600

    python conversion

---
+++ scrap.R
@@ -0,0 +1,36 @@
+### SCRAPR
+
+
+library(XML)
+library(stringr)
+
+nhl<-NULL
+years <- seq(from=1920,to=2017)
+years <- years[years!=2005]
+
+urls.list <- c()
+for(year in years){
+  urls.list <- c(urls.list, paste("http://www.hockey-reference.com/leagues/NHL_",year,"_games.html", sep=''))
+}
+
+for (i in 1:length(urls.list)){
+  table <- GET(urls.list[i])
+  tables <- readHTMLTable(rawToChar(table$content))
+  n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
+  temp<-tables[[which.max(n.rows)]]
+  nhl<-rbind(nhl,temp)
+  print(paste(years[i], ': Complete'))
+}
+
+names(nhl)<-c("Date","Visitor","VisGoals","Home","HomeGoals","OTCat","Notes")
+table(nhl$OTCat)
+nhl<-nhl[nhl$OTCat!="Get Tickets",]
+nhl$OT<-nhl$OTCat=="OT"|nhl$OTCat=="SO"
+
+write.csv(nhl, file = 'head_to_head_1920_2017.csv', row.names = F)
+
+
+table <- GET('https://www.hockey-reference.com/leagues/NHL_2018.html#all_stats')
+tables <- readHTMLTable(rawToChar(table$content))
+n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
+temp<-tables[[which.max(n.rows)]]
